5c8b4f0d7891b9f72dd10510a210c45cceac8508
JayeshPaunikar1997 <jap452@cornell.edu>
2022-03-14 16:22:16 -0400

    revert it in sometime

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import Agreement from './components/agreement';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hi from './components/hi';
 
 class App extends Component{
 
     handleSubmit = (character) => {
-        this.setState({characters: [...this.state.characters, character]})
+        // this.setState({characters: [...this.state.characters, character]});
+        console.log(character['name']);
+        console.log(character['netId']);
+        fetch('http://localhost:3000/userconsent', {
+            method: 'POST',
+            mode: 'cors', 
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(character),
+        })
     }
 
     render() {
         return (
-            <div className="App">
-                <p>{this.state}</p>
-                <Agreement initialState={this.state} handleSubmit={this.handleSubmit}/>
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Agreement initialState={this.state} handleSubmit={this.handleSubmit}/>}/>
+                    <Route path="/hi" element={<Hi />}/>
+                </Routes>
+            </BrowserRouter>
         );
     }
 }
 
-export default App
+export default App;
---
+++ src/components/hi.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class Hi extends Component{
+    render(){
+        return(
+            <p>Hi</p>
+        )
+    }
+}
+export default Hi;
