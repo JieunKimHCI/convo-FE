19b7870784b9a90d7cdb0b94ee2b178bb61c9505
Anjali Kulkarni <avk57@cornell.edu>
2022-11-17 12:27:36 -0500

    modify and add checkbox for instructions page

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,6 +1,12 @@
 import { useNavigate } from "react-router-dom";
-    
+import { useState } from "react";
+ 
 function Instructions(){
+
+    const [check1, setcheck1] = useState(false);
+    const [check2, setcheck2] = useState(false);
+    const [check3, setcheck3] = useState(false);
+    const [check4, setcheck4] = useState(false);
 
     const navigate = useNavigate();
     
@@ -20,7 +26,7 @@ function Instructions(){
     }
 
     const padding_top = {
-        paddingTop : '2vh',
+        paddingTop : '3vh',
     }
 
     const bulletStyle = {
@@ -29,34 +35,59 @@ function Instructions(){
     }
 
     const innerBoxStyle = {
-        width : '70%',
+        paddingTop: '11vh',
+        width : '80%',
     }
 
-    const nextButtonStyle = {
+    const nextButtonEnabledStyle = {
         backgroundColor: '#282c34',
         color: 'white',
         border: 'none',
         cursor: 'pointer',
-        width: '40%',
+        width: '99%',
         padding: '2vh',
     }
     
+    const nextButtonDisabledStyle = {
+        backgroundColor: 'grey',
+        color: 'white',
+        border: 'none',
+        cursor: 'pointer',
+        width: '99%',
+        padding: '2vh',
+    }
+
     function nextPage(){
         navigate('/user-consent')
+    }
+
+    function handleCheckboxInputChange(event) {
+        const name = event.target.id
+        const value = event.target.checked
+        if(name === 'check1') setcheck1(value)
+        else if(name === 'check2') setcheck2(value)
+        else if(name === 'check3') setcheck3(value)
+        else setcheck4(value)
     }
 
     return(
         <div style={instructionsPopupStyle} id = 'instructions'>
             <center>
                 <div style={innerBoxStyle}>
-                    <h2>Please follow the instructions carefully and check the box next to each instruction</h2>
+                    <h3>Please follow the instructions carefully and check the box next to each instruction</h3>
                     <ol>
-                        <li style={bulletStyle}>Enable Microphone access on your browser when prompted</li>
-                        <li style={bulletStyle}>Fill out the consent form to start using the software and participate in the study</li>
-                        <li style={bulletStyle}>Enter the meeting ID given by the researcher</li>
-                        <li style={bulletStyle}><b>PLEASE AVOID GOING ON MUTE</b></li>
+                        <li style={bulletStyle}><input type="checkbox" id="check1" name="check1" value={check1} onChange={handleCheckboxInputChange}/>Enable Microphone access on your browser when prompted</li>
+                        <li style={bulletStyle}><input type="checkbox" id="check2" name="check2" value={check2} onChange={handleCheckboxInputChange}/>Fill out the consent form to start using the software and participate in the study</li>
+                        <li style={bulletStyle}><input type="checkbox" id="check3" name="check3" value={check3} onChange={handleCheckboxInputChange}/>Enter the meeting ID given by the researcher</li>
+                        <li style={bulletStyle}><input type="checkbox" id="check4" name="check4" value={check4} onChange={handleCheckboxInputChange}/><b>PLEASE AVOID GOING ON MUTE</b></li>
                     </ol>
-                    <button style={nextButtonStyle} onClick={nextPage}>Begin</button>
+                    <input 
+                        style = {(!check1 || !check2 || !check3 || !check4) ? nextButtonDisabledStyle : nextButtonEnabledStyle } 
+                        type="button" 
+                        value="Begin" 
+                        onClick={nextPage}
+                        disabled = {(!check1 || !check2 || !check3 || !check4)} 
+                    />   
                 </div>      
             </center>  
         </div>
