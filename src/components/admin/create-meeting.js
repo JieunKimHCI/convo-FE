fceb669cfea19f914cce42890e8a35f76e10fae9
“Swathi <ss3433@cornell.edu>
2023-03-30 21:09:05 -0400

    meetingid autogeneration

--- src/components/admin/create-meeting.js
+++ src/components/admin/create-meeting.js
@@ -5,7 +5,8 @@ import { restUrl } from "../..";
 function CreateMeeting() {
 
     const navigate = useNavigate();
-    const [meetingId, setMeetingId] = useState("");
+    // const [meetingId, setMeetingId] = useState("");
+    const [meetingName, setMeetingName] = useState("");
     const [taskId, setTaskId] = useState("");
 
     const loginPopupStyle = {
@@ -73,7 +74,8 @@ function CreateMeeting() {
 
     function handleTextInputChange(event) {
         const value = event.target.value
-        setMeetingId(value)
+        // setMeetingId(value)
+        setMeetingName(value)
         setTaskId("");
     }
 
@@ -96,12 +98,14 @@ function CreateMeeting() {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    'meetingId': meetingId,
+                    // 'meetingId': meetingId,
+                    'meetingName': meetingName,
                     'taskId': taskId
                 }),
             })
                 .then(response => {
                     if (response.status === 200) {
+                        const meetingId = response.meetingId;
                         navigate('/admin', {
                             state: {
                                 meetingId: meetingId,
@@ -126,36 +130,36 @@ function CreateMeeting() {
             <center>
                 <form style={innerBoxStyle}>
                     <div style={padding_top}>
-                        <b>Meeting ID</b>&nbsp;&nbsp;
-                        <input id='meetingId' name='meetingId' onChange={handleTextInputChange} />
+                        <b>Meeting Name</b>&nbsp;&nbsp;
+                        <input id='meetingName' name='meetingName' onChange={handleTextInputChange} />
                     </div>
                     <div style={taskButtonStyle}>
                         <input
                             id='desertTask'
-                            style={meetingId === "" || taskId === 1 ? taskButtonDisabledStyle : taskButtonEnabledStyle}
+                            style={meetingName === "" || taskId === 1 ? taskButtonDisabledStyle : taskButtonEnabledStyle}
                             type="button"
                             value="Select Desert Task"
                             onClick={handleTaskDesertChange}
-                            disabled={meetingId === ""}
+                            disabled={meetingName === ""}
                         />
                     </div>
                     <div style={taskButtonStyle}>
                         <input
                             id='hiddenTask'
-                            style={meetingId === "" || taskId === 0 ? taskButtonDisabledStyle : taskButtonEnabledStyle}
+                            style={meetingName === "" || taskId === 0 ? taskButtonDisabledStyle : taskButtonEnabledStyle}
                             type="button"
                             value="Select Hidden Task"
                             onClick={handleTaskHiddenChange}
-                            disabled={meetingId === ""}
+                            disabled={meetingName === ""}
                         />
                     </div>
                     <div style={{ paddingTop: '21vh' }}>
                         <input
-                            style={taskId === "" || meetingId === "" ? loginButtonDisabledStyle : loginButtonEnabledStyle}
+                            style={taskId === "" || meetingName === "" ? loginButtonDisabledStyle : loginButtonEnabledStyle}
                             type="button"
                             value="Create Meeting"
                             onClick={createMeeting}
-                            disabled={meetingId === "" || taskId === ""}
+                            disabled={meetingName === "" || taskId === ""}
                         />
                     </div>
                 </form>
